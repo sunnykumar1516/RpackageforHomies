d1eaddff4524a520df222bf0901eec2322858235
sunny  kumar <sunnykumar1516@gmail.com>
2023-01-25 20:34:44 +0000

    changes in helper and some turkish function name addition.

--- R/helper.R
+++ R/helper.R
@@ -3,23 +3,23 @@
 #' creates histogram
 #'
 #' @param bhai_use_this_data pass data
-#' @param use_this_column pass the column
 #' @param Bhai_use_this_colour pass the colour
 #' @param Bhai_use_this_binwidth  pass the bin-width here
+#' @param bhai_use_this_column  pass column to be used here
 #'
 #' @return  it returns the graph drawn
 #' @export
 #'
 #' @examples bhai_create_histogram(bhai_use_this_data=cars,
-#' use_this_column=dist,
+#' bhai_use_this_column=dist,
 #' Bhai_use_this_colour='red',
 #' Bhai_use_this_binwidth = 3 )
 #'
 bhai_create_histogram<- function(bhai_use_this_data,
-                                 use_this_column,
+                                 bhai_use_this_column,
                                  Bhai_use_this_colour="#69b3a2",
                                  Bhai_use_this_binwidth=5){
-  ggplot2::ggplot(bhai_use_this_data, ggplot2::aes(x={{use_this_column}})) +
+  ggplot2::ggplot(bhai_use_this_data, ggplot2::aes(x={{bhai_use_this_column}})) +
   ggplot2::geom_histogram( binwidth=Bhai_use_this_binwidth,
                              fill=Bhai_use_this_colour,
                              color="green",
@@ -45,7 +45,9 @@ bhai_create_histogram<- function(bhai_use_this_data,
 #'
 
 #'
-bhai_create_scatterplot<- function(bhai_use_this_data,xData,yData
+bhai_create_scatterplot<- function(bhai_use_this_data,
+                                   xData,
+                                   yData
                                    ,bhai_use_this_category_for_colour){
   ggplot2::ggplot(bhai_use_this_data, ggplot2::aes(x={{xData}}, y={{yData}},
                                                    color={{bhai_use_this_category_for_colour}})) +
@@ -71,11 +73,100 @@ bhai_create_scatterplot<- function(bhai_use_this_data,xData,yData
 #' yData = Sepal.Length
 #' ,bhai_use_this_category_for_colour=Species)
 #'
-bhai_create_boxplot<- function(bhai_use_this_data,xData,yData
-                                   ,bhai_use_this_category_for_colour){
+bhai_create_boxplot<- function(bhai_use_this_data,
+                               xData,
+                               yData,
+                               bhai_use_this_category_for_colour){
   ggplot2::ggplot(bhai_use_this_data, ggplot2::aes(x={{xData}}, y={{yData}},
                                                    fill={{bhai_use_this_category_for_colour}})) +
     ggplot2::geom_boxplot()
+
+
+}
+
+
+
+
+#' oluşturmak Histogram
+#'
+#' @param kanka_use_this_data  pass data
+#' @param kanka_use_this_column pass column
+#' @param kanka_use_this_renk pass renk
+#' @param kanka_use_this_binwidth pass binwidth
+#'
+#' @return returns graph
+#' @export
+#'
+#' @examples kanka_create_histogram (cars,
+#' dist,
+#''red',
+#' 3 )
+kanka_create_histogram <- function(kanka_use_this_data,
+                                       kanka_use_this_column,
+                                       kanka_use_this_renk="#69b3a2",
+                                       kanka_use_this_binwidth=5){
+
+  bhai_create_histogram(kanka_use_this_data,
+                        {{kanka_use_this_column}},
+                        kanka_use_this_renk,
+                        kanka_use_this_binwidth
+                       )
+
+}
+
+
+#' oluşturmak Scatterplot
+#'
+#' @param kanka_use_this_data pass data
+#' @param xData               data on x axis
+#' @param kanka_use_this_category_for_renk pass colour
+#' @param yData               data on y axis
+#'
+#' @return returns graph
+#' @export
+#'
+#' @examples kanka_create_scatterplot(iris,
+#' Sepal.Length,
+#' Sepal.Width,
+#' Species)
+kanka_create_scatterplot<- function(kanka_use_this_data,
+                                   xData,
+                                   yData
+                                   ,kanka_use_this_category_for_renk){
+
+  bhai_create_scatterplot(kanka_use_this_data,
+                          {{xData}},
+                          {{yData}},
+                          {{kanka_use_this_category_for_renk}})
+
+}
+
+
+
+#' oluşturmak Boxplot
+#'
+#' @param kanka_use_this_data pass data
+#' @param xData               data on x-axis
+#' @param kanka_use_this_category_for_renk pass category
+#' @param yData               data on y-axis
+#'
+#' @return returns graph
+#' @export
+#'
+#' @examples kanka_create_boxplot(iris,
+#' xData =  Species,
+#' yData = Sepal.Length,
+#' Species)
+kanka_create_boxplot<- function(kanka_use_this_data,
+                               xData,
+                               yData,
+                               kanka_use_this_category_for_renk) {
+
+
+bhai_create_boxplot(kanka_use_this_data,
+                                 {{xData}},
+                                 {{yData}},
+                                 {{kanka_use_this_category_for_renk}})
 
 
 }
